9bb9ea72d9ac2045fb4618fb4264bd1f4ed22eb3
Nir <nbtk123@gmail.com>
2016-05-07 00:03:48 +0300

    bar rafaelli is showing in ads

--- chrome_extension/swap_ad.js
+++ chrome_extension/swap_ad.js
@@ -2,25 +2,27 @@ function swap(new_ad_url) {
 	//var swapped_ad = document.getElementById('ads.ozen.right');
 	var swapped_ad = $('#ads.ozen.right');
     if (swapped_ad) {
-//		alert('good');
+		alert('good1');
 		$("#ads.ozen.right").hide();
 		$('body').append('<img src="' + new_ad_url + '" style="position:fixed;top:0;right:0;" />');
+		//$('body').append('<img src="http://10.10.20.152:8081/ads/1347434767_1.jpeg" style="position:fixed;top:0;right:0;" />');
 	} else {
-//		alert('bad');
+		alert('bad1');
 		setTimeout(function() {swap(new_ad_url)}, 500);
 	}
 }
 
 $.ajax({
     type: 'GET',
-    //url: 'http://localhost:8081/get_ad',
-    url: 'http://localhost:8081/',
-    //user_id : '123234',
+    url: 'http://10.10.20.152:8081/',
     crossDomain: true,
+    
+    data: {user_id: 1347434767},
     
     success: function (response) {
         alert('good');
-        new_ad_url = 'http://cdn.static-economist.com/sites/default/files/images/articles/migrated/20101211_tqp001_0.jpg';
+        console.log(response);
+        new_ad_url = response;
         swap(new_ad_url);
     },
     
