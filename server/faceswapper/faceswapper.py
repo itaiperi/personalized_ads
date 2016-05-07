9a84eb36fadfaeab4c31065a517932d7106f6cc8
noodlez04 <noodlez040@gmail.com>
2016-05-07 11:01:45 +0300

    Add ad tester to check if ad is valid for swap. Bugfix faceswapper

--- server/faceswapper/faceswapper.py
+++ server/faceswapper/faceswapper.py
@@ -191,23 +191,27 @@ def correct_colours(im1, im2, landmarks1):
     return (im2.astype(numpy.float64) * im1_blur.astype(numpy.float64) /
                                                 im2_blur.astype(numpy.float64))
 
-im1, landmarks1 = read_im_and_landmarks(OWN_PATH + sys.argv[2])
-im2, landmarks2 = read_im_and_landmarks(OWN_PATH + sys.argv[1])
+def swap(ad_img, user_img):
+    im1, landmarks1 = read_im_and_landmarks(ad_img)
+    im2, landmarks2 = read_im_and_landmarks(user_img)
 
-M = transformation_from_points(landmarks1[ALIGN_POINTS],
-                               landmarks2[ALIGN_POINTS])
+    M = transformation_from_points(landmarks1[ALIGN_POINTS],
+                                   landmarks2[ALIGN_POINTS])
 
-mask = get_face_mask(im2, landmarks2)
-warped_mask = warp_im(mask, M, im1.shape)
-combined_mask = numpy.max([get_face_mask(im1, landmarks1), warped_mask],
-                          axis=0)
+    mask = get_face_mask(im2, landmarks2)
+    warped_mask = warp_im(mask, M, im1.shape)
+    combined_mask = numpy.max([get_face_mask(im1, landmarks1), warped_mask],
+                              axis=0)
 
-warped_im2 = warp_im(im2, M, im1.shape)
-warped_corrected_im2 = correct_colours(im1, warped_im2, landmarks1)
+    warped_im2 = warp_im(im2, M, im1.shape)
+    warped_corrected_im2 = correct_colours(im1, warped_im2, landmarks1)
 
-output_im = im1 * (1.0 - combined_mask) + warped_corrected_im2 * combined_mask
+    output_im = im1 * (1.0 - combined_mask) + warped_corrected_im2 * combined_mask
 
-user_filename = sys.argv[1].split("/")[-1].split(".")[0]
-ad_filename = sys.argv[2].split("/")[-1].split(".")[0]
+    user_filename = user_img.split("/")[-1].split(".")[0]
+    ad_filename = ad_img.split("/")[-1].split(".")[0]
 
-cv2.imwrite(OWN_PATH + '../resources/images/personalized_ads/' + user_filename + '_' + ad_filename + '.jpg', output_im)
+    cv2.imwrite(OWN_PATH + '../resources/images/personalized_ads/' + user_filename + '_' + ad_filename + '.jpg', output_im)
+
+if __name__ == "__main__":
+    swap(OWN_PATH + sys.argv[2], OWN_PATH + sys.argv[1])
